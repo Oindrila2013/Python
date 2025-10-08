2749b039751bad254cda71b264e0a08dab87c36f
Oindrila2013 <palash.banik@gmail.com>
2025-10-08 22:14:13 +0600

    polygon using python

--- polygon.py
+++ polygon.py
@@ -5,6 +5,23 @@ turtle.Screen().bgcolor("#AEE5F4")
 sc = turtle.Screen()
 sc.setup(400, 300)
 
-turtle.title("Welcome to the turtle window")
+board = turtle.Turtle()
+
+# triangle
+board.forward(100)
+board.left(120)
+board.forward(100)
+board.left(120)
+board.forward(100)
+
+# square
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
 
 turtle.done()
