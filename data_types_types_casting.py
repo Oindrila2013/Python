52b7e5b0bbd93803c1ffeabfb040500f515926a5
Oindrila2013 <palash.banik@gmail.com>
2025-10-08 21:34:55 +0600

    data_types_types_casting

---
+++ data_types_types_casting.py
@@ -0,0 +1,12 @@
+name = "Ankita"
+age = 12
+is_student = True
+weight = 39.80
+
+print("Type of name", type(name))
+
+print("Type of age", type(age))
+
+print("Type of is_student", type(is_student))
+
+print("Type of weight", type(weight))
