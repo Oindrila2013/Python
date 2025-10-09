e65deef337d13b93f950bd1ad3d1b6fb5c15ec56
Oindrila2013 <palash.banik@gmail.com>
2025-10-09 22:31:09 +0600

    conversation_with_strings

---
+++ conversation_with_strings.py
@@ -0,0 +1,15 @@
+name = "Ankita"
+age = 12
+is_student = True
+weight = 41.30
+like = "coding"
+
+print("My name is:", (name))
+
+print("My age is:", (age))
+
+print("I am a student:", (is_student))
+
+print("My weight is:", (weight))
+
+print("My favourite thing to do is:", (like))
