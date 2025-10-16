6616934857ce580b631691bccd4a44e1dc853d5e
Oindrila2013 <palash.banik@gmail.com>
2025-10-16 22:12:41 +0600

    Functions

---
+++ palindrome.py
@@ -0,0 +1,13 @@
+def ispalindrome(string):
+    left_pos = 0
+    right_pos = len(string) -1
+    while right_pos <= left_pos:
+        if not string[right_pos] == string[left_pos]:
+            return False
+        left_pos +=1
+        right_pos -=1
+    return True
+
+print("Is this a palindrome string ?")
+print(ispalindrome("malaylam"))
+        
