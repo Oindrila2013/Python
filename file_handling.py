891d607a099a5c73604508b298b109ace8a6634e
Oindrila2013 <palash.banik@gmail.com>
2025-11-19 22:44:05 +0600

    File Handling

---
+++ file_handling.py
@@ -0,0 +1,19 @@
+file = open("codingal1.txt", "r")
+
+print(file.read())
+
+file.close()
+
+
+file = open("codingal.txt", "w")
+
+file.write("Hello, this is Ankita. I am a student of codingal")
+
+file.close()
+
+
+file = open("codingal.txt", "a")
+
+file.write("I love learning coding. It is so much fun.")
+
+file.close()
